c05b631be78eff1295080d895faad2a0e43c2cb8
rangamach <117797904+rangamach@users.noreply.github.com>
2024-10-29 19:16:20 +0530

    Choose First Pokemon

--- Pokemon/Pokemon/Pokemon.cpp
+++ Pokemon/Pokemon/Pokemon.cpp
@@ -2,10 +2,57 @@
 
 using namespace std;
 
+static string EnterName()
+{
+    string name;
+    cout << "Enter your Name: ";
+    cin >> name;
+    return name;
+}
+
+static void WelcomeMessage(string name)
+{
+    cout << "Welcome " + name + "!\n";
+}
+
+static void ChooseFirstPokemon()
+{
+    cout << "Welcome to the world of Pokemon! I am Professor Oak.\n";
+    cout << "You can choose one of the following Pokemon:\n";
+    cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+    cout << "Which Pokemon would you like to choose? Enter the number: ";
+
+    int choice;
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 1:
+            cout << "You chose Bulbasaur! A wise choice.\n";
+            break;
+        case 2:
+            cout << "You chose Charmander! A fiery choice.\n";
+            break;
+        case 3:
+            cout << "You chose Squirtle! A cool choice.\n";
+            break;
+        default:
+            cout << "Invalid choice. Please restart the game.\n";
+            break;
+    }
+
+    cout << "Ah, an excellent choice!\n";
+    cout << "But beware, Trainer,\n";
+    cout << "this is only the beginning.\n";
+    cout << "Your journey is about to unfold.\n";
+    cout << "Now let's see if you've got what it takes to keep going!\n";
+    cout << "Good luck, and remember. . . Choose Wisely!\n";
+}
+
+
+
 int main()
 {
-    string player_name;
-    cout << "Enter your Name: ";
-    cin >> player_name;
-    cout << "Welcone to world of Pokemon " + player_name + "!";
+    WelcomeMessage(EnterName());
+    ChooseFirstPokemon();
 }
