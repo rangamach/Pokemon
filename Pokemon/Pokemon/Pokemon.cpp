19b0fc979177ad647ea0bbf354a01ef95392c7ce
rangamach <117797904+rangamach@users.noreply.github.com>
2024-10-31 11:18:17 +0530

    Added the Game Loop.

--- Pokemon/Pokemon/Pokemon.cpp
+++ Pokemon/Pokemon/Pokemon.cpp
@@ -204,43 +204,46 @@ void GameLoop(Player &player)
 {
     int choice;
     bool keepPlaying = true;
-    ClearConsole();
-    cout << "\nWhat would you like to do next, " << player.name << "?\n";
-    cout << "1. Battle Wild Pokemon\n";
-    cout << "2. Visit PokeCenter\n";
-    cout << "3. Challenge Gyms\n";
-    cout << "4. Enter Pokemon League\n";
-    cout << "5. Quit\n";
-    cout << "Enter your choice: ";
-    cin >> choice;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    switch (choice)
+    while (keepPlaying)
     {
-    case 1:
-        cout << "You look around... but all the wild Pokémon are on vacation. Maybe try again later?\n";
-        break;
-    case 2:
-        cout << "You head to the PokeCenter, but Nurse Joy is out on a coffee break. Guess your Pokemon will have to tough it out for now!\n";
-        break;
-    case 3:
-        cout << "You march up to the Gym, but it's closed for renovations. Seems like even Gym Leaders need a break!\n";
-        break;
-    case 4:
-        cout << "You boldly step towards the Pokémon League... but the gatekeeper laughs and says, 'Maybe next time, champ!'\n";
-        break;
-    case 5:
-        cout << "You try to quit, but Professor Oak's voice echoes: 'There's no quitting in Pokémon training!'\n";
-        cout << "Are you sure you want to quit? (y/n): ";
-        char quitChoice;
-        cin >> quitChoice;
-        if (quitChoice == 'y' || quitChoice == 'Y')
-            keepPlaying = false;
-        break;
-    default:
-        cout << "That's not a valid choice. Try again!\n";
-        break;
-    };
-    WaitForEnter();
+        ClearConsole();
+        cout << "\nWhat would you like to do next, " << player.name << "?\n";
+        cout << "1. Battle Wild Pokemon\n";
+        cout << "2. Visit PokeCenter\n";
+        cout << "3. Challenge Gyms\n";
+        cout << "4. Enter Pokemon League\n";
+        cout << "5. Quit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        switch (choice)
+        {
+        case 1:
+            cout << "You look around... but all the wild Pokemon are on vacation. Maybe try again later?\n";
+            break;
+        case 2:
+            cout << "You head to the PokeCenter, but Nurse Joy is out on a coffee break. Guess your Pokemon will have to tough it out for now!\n";
+            break;
+        case 3:
+            cout << "You march up to the Gym, but it's closed for renovations. Seems like even Gym Leaders need a break!\n";
+            break;
+        case 4:
+            cout << "You boldly step towards the Pokémon League... but the gatekeeper laughs and says, 'Maybe next time, champ!'\n";
+            break;
+        case 5:
+            cout << "You try to quit, but Professor Oak's voice echoes: 'There's no quitting in Pokemon training!'\n";
+            cout << "Are you sure you want to quit? (y/n): ";
+            char quitChoice;
+            cin >> quitChoice;
+            if (quitChoice == 'y' || quitChoice == 'Y')
+                keepPlaying = false;
+            break;
+        default:
+            cout << "That's not a valid choice. Try again!\n";
+            break;
+        };
+        WaitForEnter();
+    }
     cout << "Goodbye, " << player.name << "! Thanks for playing!\n";
 }
 
